b64e11cb2636f4401de24261adbdbd1cad38e05b
root <root@ip-10-0-11-140.ec2.internal>
2026-08-25 07:53:37 +0000

    Aded

--- Day-6-statelock/main.tf
+++ Day-6-statelock/main.tf
@@ -16,3 +16,20 @@ resource "aws_subnet" "public_subnet_1a" {
   }
 
 }
+
+resource "aws_vpc" "my_vpc2" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "My-Vpc"
+  }
+}
+
+
+resource "aws_vpc" "my_vpc3" {
+  cidr_block = "10.20.0.0/16"
+
+  tags = {
+    Name = "My-Vpc2"
+  }
+}
